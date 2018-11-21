743f97bb0ff13b0d50321dfb3ad133e81f80fc27
lmarkdev <logan_markewich@mentor.com>
2018-11-21 14:19:44 -0800

    Added ability to display timing attributes in path

--- python/liberty_pathing.py
+++ python/liberty_pathing.py
@@ -6,6 +6,7 @@ import gzip
 
 from anytree import Node, RenderTree, search
 
+
 def index_file(filename):
   stack = list()
   tree = list()
@@ -18,13 +19,38 @@ def index_file(filename):
   print("Indexing " + filename)  
   
   open_func = gzip.open if '.gz' in filename else open
-    
-  with open_func(filename, 'r') as f:
-    for line in f:
-      line_num += 1
-      # start of branch
-      if '{' in line:    
+  f = open_func(filename, 'r')  
+  for line in f:
+    line_num += 1
+    if '{' in line:    
+      if 'timing ()' in line or 'timing()' in line:
+        true_line = line_num
+        attributes = set() 
+        while True:
+          line = f.next()
+          line_num += 1
+          if ':' in line:
+            attributes.add(line.strip())
+          else:
+            break
+
+        # Build up timing name with attributes
+        name = 'timing('
+        for attribute in attributes:
+          name += attribute.replace(' ', '').replace(':', '=').replace(';', '') + ', '
+        name = name.strip().strip(',') + ')'
+
+        tree.append(Node(name, parent=stack[-1], startLine=true_line))
+        stack.append(tree[-1])
+
+        # Need to check if current line is a new group (it probably is if we just got timing attributes)
+        if '{' in line:
+          name = line.strip().split('{')[0].strip()
+          tree.append(Node(name, parent=stack[-1], startLine=line_num))
+          stack.append(tree[-1])
+      else:  
         name = line.strip().split('{')[0].strip()
+
         if is_root:
           tree.append(Node(name, startLine=line_num))
           stack.append(tree[0])
@@ -32,16 +58,17 @@ def index_file(filename):
         else:
           tree.append(Node(name, parent=stack[-1], startLine=line_num))
           stack.append(tree[-1])
-      # end of branch
-      elif '}' in line:
-        index = tree.index(stack[-1])
-        tree[index].endLine = line_num
-        stack.pop()
+
+
+    elif '}' in line:
+      index = tree.index(stack[-1])
+      tree[index].endLine = line_num
+      stack.pop()
   print("Indexing Complete!")  
   return tree
+
 
 def show_branching(tree):
   row, col = vim.current.window.cursor
   print(str(search.findall(tree[0], filter_=lambda node: node.startLine <= row and node.endLine >= row)[-1]).split("'")[1].replace(" ",""))
-  #print(RenderTree(tree[0]))
     
