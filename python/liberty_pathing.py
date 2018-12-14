c58bb681517256708ee24b1451b91d14b096a7b0
lmarkdev <logan_markewich@mentor.com>
2018-12-13 19:28:18 -0800

    Minor indexing bug fixes + added more comments

--- python/liberty_pathing.py
+++ python/liberty_pathing.py
@@ -1,44 +1,55 @@
-import urllib2
-import json
 import vim
-import time
 import gzip
 import os.path
 import imp
 
+from imp import load_source
 from anytree import Node, RenderTree, search
 from os.path import expanduser
 
-# Use parsers if available
+# Import parsers (if defined)
 all_parsers = None
 cell_name_parser = None
 unified_parser_file = os.path.expandvars('$SOLIDO_EXTERNAL_PARSERS')
 cell_parser_file = os.path.expandvars('$SOLIDO_CELL_NAME_PARSER')
 
 if unified_parser_file != '$SOLIDO_EXTERNAL_PARSERS':
-  all_parsers = imp.load_source(unified_parser_file.split('/')[-1].split('.')[0], unified_parser_file)
+  all_parsers = load_source(unified_parser_file.split('/')[-1].split('.')[0], unified_parser_file)
 elif cell_parser_file != '$SOLIDO_CELL_NAME_PARSER':
-  cell_name_parser = imp.load_source(cell_parser_file.split('/')[-1].split('.')[0], cell_parser_file)
+  cell_name_parser = load_source(cell_parser_file.split('/')[-1].split('.')[0], cell_parser_file)
 
+# Constants
 NO_ATTRIBUTES = ['cell', 'library', 'pin', 'bus', 'bundle', 'propagated_noise_low', 'propagated_noise_high', 'output_voltage_high', 'output_voltage_low']
 IGNORE_ATTRIBUTES = ['value', 'sdf_cond', 'miller_cap_fall', 'miller_cap_rise', 'is_needed', 'stage_type', 'is_inverting']
 
+
+# Function to index liberty file into a tree structure
+# Inputs:
+#   filename  - Path to a Liberty file to open.
+# Outputs:
+#   tree      - Set of anytree nodes containing indexed Liberty file locations.
+#               Each node contains the name of the group, start line of the group, and end line
+#               Will also return None if it is not a .lib or .lib.gz file
 def index_file(filename):
   stack = list()
   tree = list()
   is_root = True
   line_num = 0
   
-  if not(filename.endswith('.lib') or filename.endswith('.gz')):
-    return None, None
+  # Check file type
+  if not(filename.endswith('.lib') or filename.endswith('.lib.gz')):
+    return None
   
   print("Indexing " + filename)  
   
+  # Pick open function
   open_func = gzip.open if filename.endswith(".gz") else open
+  
   f = open_func(filename, 'r')  
   for line in f:
     line_num += 1
     if '{' in line:    
+      # Do we need to check for attributes?
       if not line.split('(')[0].strip() in NO_ATTRIBUTES:
         group = line.split('(')[0].strip()
         true_line = line_num
@@ -46,12 +57,15 @@ def index_file(filename):
         while True:
           line = f.next()
           line_num += 1
+          # Grab attributes (they have : in them) or grab indexes for vectors
           if ':' in line or ('index' in line and 'vector' in group):
-            attributes.add(line.strip())
+            attributes.add(line.strip().replace('\t',''))
+          elif line.strip() == '':
+            continue
           else:
             break
 
-        # Build up timing name with attributes
+        # Build up group name with attributes
         name = group.split('(')[0].strip() + '{'
         for attribute in attributes:
           if ':' in attribute:
@@ -60,22 +74,25 @@ def index_file(filename):
           else:
             name += attribute.replace(' ', '').replace(';', '').replace('"','') + ', '
         name = name.strip().strip(',') + '}'
-
-        tree.append(Node(name, parent=stack[-1], startLine=true_line))
+        
+        # Add group node to tree
+        tree.append(Node(name, parent=stack[-1], start_line=true_line))
         stack.append(tree[-1])
 
-        # Need to check if current line is a new group (it probably is if we just got timing attributes)
+        # Need to check if current line is a new group (it probably is if we just got group attributes)
         if '{' in line:
           name = line.strip().split('{')[0].strip()
-          tree.append(Node(name, parent=stack[-1], startLine=line_num))
+          tree.append(Node(name, parent=stack[-1], start_line=line_num))
           stack.append(tree[-1])
         elif '}' in line:
           index = tree.index(stack[-1])
-          tree[index].endLine = line_num
+          tree[index].end_line = line_num
           stack.pop()
-      else:  
-        name = line.strip().split('{')[0].strip()
-      
+      else:
+        # Add a group that doesn't need attributes
+        name = line.strip().split('{')[0].strip() 
+
+        # Check if we need (or can) use the cell name parser
         if 'cell' in name:
           cell = name.split('(')[1].strip(')').strip()
           if all_parsers != None:
@@ -92,37 +109,55 @@ def index_file(filename):
               name = line.strip().split('{')[0].strip()
         if is_root:
           name = name.split('(')[0]
-          tree.append(Node(name, startLine=line_num))
+          tree.append(Node(name, start_line=line_num))
           stack.append(tree[0])
           is_root = False
         else:
-          tree.append(Node(name, parent=stack[-1], startLine=line_num))
+          tree.append(Node(name, parent=stack[-1], start_line=line_num))
           stack.append(tree[-1])
-
-
     elif '}' in line:
+      # Reached end of group, record endline and remove from stack
       index = tree.index(stack[-1])
-      tree[index].endLine = line_num
+      tree[index].end_line = line_num
       stack.pop()
+
+  f.close()
   print("Indexing Complete!")  
   return tree
 
 
+# Function show user current Liberty location to user.
+# Inputs:
+#   tree  - Set of anytree nodes containing indexed Liberty file locations.
 def show_branching(tree):
   row, col = vim.current.window.cursor
-  print(str(search.findall(tree[0], filter_=lambda node: node.startLine <= row and node.endLine >= row)[-1]).split("'")[1].replace(" ",""))
+  print(str(search.findall(tree[0], filter_=lambda node: node.start_line <= row and node.end_line >= row)[-1]).split("'")[1].replace(" ",""))
 
 
-def _get_file_len(fname):
-  with open(fname) as f:
+# Helper function to get length of a file in lines
+# Inputs:
+#   filename  - Path to a file to open.
+# Outputs:
+#   i         - Length of file in lines.
+def _get_file_len(filename):
+  with open(filename) as f:
     for i, l in enumerate(f):
       pass
   return i + 1
 
 
+# Function to save the current Liberty location into a bookmark file for use in MLChar.
+# Multiple bookmarks made in the same Liberty file will appear in the same bookmark.
+# Inputs:
+#   tree      - Set of anytree nodes containing indexed Liberty file locations.
+#   filename  - Path to a Liberty file to open.
+# Outputs:
+#   A file in your home directory with the saved bookmark. 
 def save_location(tree, filename):
   row, col = vim.current.window.cursor
-  location = str(search.findall(tree[0], filter_=lambda node: node.startLine <= row and node.endLine >= row)[-1]).split("'")[1].replace(" ","")
+  
+  # Get the name of the node that is in the current location
+  location = str(search.findall(tree[0], filter_=lambda node: node.start_line <= row and node.end_line >= row)[-1]).split("'")[1].replace(" ","")
   location_list = location.split('/')
 
   # Formate end of location for bookmark
@@ -132,6 +167,7 @@ def save_location(tree, filename):
   # Build the bookmark entry
   base_name = filename.split('/')[-1]
   library = base_name.split('.lib')[0]
+  
   bookmark = ''
   for item in location_list:
     bookmark += item + '/'
@@ -140,8 +176,10 @@ def save_location(tree, filename):
   bookmark += ',,,,ID'
   bookmark = '"' + bookmark
 
-  # Check if bookmark csv already exists
-  bookmark_file_path = home = expanduser("~") + '/' + base_name + '_bookmark.csv'
+  # Check if bookmark csv already exists based on current Liberty file name
+  # If it exists, append the new bookmark
+  # If not, create a file with the proper header and bookmark
+  bookmark_file_path = expanduser("~") + '/' + base_name + '_bookmark.csv'
   
   if os.path.isfile(bookmark_file_path):
     file_len = _get_file_len(bookmark_file_path)
@@ -157,4 +195,5 @@ def save_location(tree, filename):
       f.write(bookmark)
       f.write('\n')
   
+  # Notify user of saved location of bookmark
   print("Bookmarked saved in " + bookmark_file_path)
